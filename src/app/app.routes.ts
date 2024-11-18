e475dae90ff5a906f1c17dd373dc1e4ef945bfe5
AdamrePL <adam1234federowicz@gmail.com>
2024-11-18 15:16:45 +0100

    update yaaay

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { KadraComponent } from './pages/kadra/kadra.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/news/news.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'aktualnosci', component: NewsComponent},
+    {path: 'news', component: NewsComponent},
+    {path: 'kadra', component: KadraComponent},
+    {path: 'staff', component: KadraComponent}
+];
